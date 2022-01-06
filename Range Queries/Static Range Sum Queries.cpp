4e12be285f45f4ee4afe65a9299f260fa0aea638
Madhav Agarwal <madhavagarwal505@gmail.com>
2022-01-06 21:35:28 +0530

    initial commit

---
+++ Range Queries/Static Range Sum Queries.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define ff first
+#define ss second
+int solve()
+{
+    
+}
+
+int main()
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        cout<<solve()<<endl;
+    }
+
+    return 0;
+}
