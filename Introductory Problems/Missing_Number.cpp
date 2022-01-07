19d9f07bd1394d6278ccf5de8686eaf3715bc29f
Madhav Agarwal <madhavagarwal505@gmail.com>
2022-01-07 17:37:13 +0530

    done

---
+++ Introductory Problems/Missing_Number.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define ff first
+#define ss second
+vector<ll> v((2e+5)+1,0);
+
+int main()
+{ 
+    ll n;
+    cin>>n;
+
+    for(ll i=1;i<n;i++)
+    {
+        ll x;
+        cin>>x;
+        v[x]=1;
+    }
+    for(ll i=1;i<v.size();i++)
+    {
+        if(v[i]!=1)
+        {
+            cout<<i;
+            break;
+        }
+    }
+
+    
+    
+    return 0;
+}
