2552bb99ddbb33e9974e0f3c3ff34383f8035315
Madhav Agarwal <madhavagarwal505@gmail.com>
2022-01-15 15:27:13 +0530

    done

---
+++ Introductory Problems/Two_Knights.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+#define ff first
+#define ss second
+
+int main()
+{ 
+    ll n;
+    cin>>n;
+
+    for(int i=1;i<=n;i++)
+    {
+        ll total_ways=(i*i*(i*i-1))/2;
+
+        ll attack_ways=4*(i-1)*(i-2);
+
+        ll safe_ways = total_ways-attack_ways;
+
+        cout<<safe_ways<<endl;
+    }
+    
+    return 0;
+}
